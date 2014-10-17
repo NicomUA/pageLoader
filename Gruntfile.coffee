885ff20f4f923526fcb264e5c5c363fbe13beea5
NicomUA <macaniki@gmail.com>
2014-10-17 18:10:40 +0300

    bootstrap theme for example grunt swig tpl compiling task

--- Gruntfile.coffee
+++ Gruntfile.coffee
@@ -9,14 +9,27 @@ module.exports = (grunt) ->
             dist:
                 files: ['dist/pageLoader.js': 'src/pageLoader.coffee']
 
+        swig:
+            dist:
+                generateSitemap: false,
+                generateRobotstxt: false,
+                dest: "."
+                src: ['src/examples/*.swig','!src/examples/layout.swig']
+
+
         watch:
             coffee:
                 files: 'src/*'
                 tasks: ['coffee','uglify']
 
-    grunt.loadNpmTasks 'grunt-contrib-uglify';
+
+            swig:
+                files: 'src/examples/*'
+                tasks: ['swig']
+
     grunt.loadNpmTasks 'grunt-contrib-uglify';
     grunt.loadNpmTasks 'grunt-contrib-coffee';
+    grunt.loadNpmTasks 'grunt-swig';
     grunt.loadNpmTasks 'grunt-contrib-watch';
 
-    grunt.registerTask 'default', ['coffee','uglify','watch'];
+    grunt.registerTask 'default', ['coffee','uglify','swig','watch'];
--- examples/js/config.js
+++ examples/js/config.js
@@ -1,24 +1,22 @@
 pageLoader.libPath = './js/';
 pageLoader.pagePath = './js/pages/';
-pageLoader.globals = ['https://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/js/bootstrap.min.js'];
+pageLoader.globals = ['https://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/js/bootstrap.min.js','https://code.jquery.com/ui/1.11.2/jquery-ui.min.js'];
 pageLoader.debug = true;
 
 // pages configuration
 pageLoader.pages = {
     '/examples/index.html': {
         'module': 'index.js',
-        'require': [
-            'https://code.jquery.com/ui/1.11.2/jquery-ui.min.js'
-        ]
+        'require': []
     },
     '/examples/accordion.html': {
         'module': 'accordion.js',
-        'require': ['https://code.jquery.com/ui/1.11.2/jquery-ui.min.js']
+        'require': []
     },
 
-    '/examples/datapicker.html': {
+    '/examples/datepicker.html': {
         'module': 'datepicker.js',
-        'require': ['https://code.jquery.com/ui/1.11.2/jquery-ui.min.js']
+        'require': []
     },
     '(.*)': {
         'module': null,
